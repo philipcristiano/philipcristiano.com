e5a4da85d41d9e794be97ee71269d6d2d294a38a
Philip Cristiano <philipcristiano@gmail.com>
2014-12-20 13:47:56 -0500

    Deploy

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import *
 from fabric.contrib.project import rsync_project
 
-env.hosts = ['root@node.philipcristiano.com']
+env.hosts = ['root@philipcristiano.com']
 
 def upload():
-    rsync_project('/var/www', 'philipcristiano')
+    rsync_project('/var/www/philipcristiano', 'output/*')
